79676bf21a84a9b79afa397eb9483d751644183a
rahul-kapuriya <rahulkapuriya25@gmail.com>
2025-06-29 16:30:12 +0530

    Changes are made

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+/* namespace std allows you to write basic std functions directly without using whole std::
+   ex. to print you have to write std::cout but using namespace you can directly write cout
+*/
+
 int main ()
 {
     cout<<"Hello World"<<endl;
